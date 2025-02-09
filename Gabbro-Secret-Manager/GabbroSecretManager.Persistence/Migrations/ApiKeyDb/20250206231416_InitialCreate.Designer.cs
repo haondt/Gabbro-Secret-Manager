ef9e70e17c635fa6050bd17b27ac233028ba6dc5
Noah <burghardtnoah@gmail.com>
2025-02-09 13:21:26 -0700

    trying .net 8 downgrade

--- Gabbro-Secret-Manager/GabbroSecretManager.Persistence/Migrations/ApiKeyDb/20250206231416_InitialCreate.Designer.cs
+++ Gabbro-Secret-Manager/GabbroSecretManager.Persistence/Migrations/ApiKeyDb/20250206231416_InitialCreate.Designer.cs
@@ -17,7 +17,7 @@ namespace GabbroSecretManager.Persistence.Migrations.ApiKeyDb
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
 
             modelBuilder.Entity("GabbroSecretManager.Persistence.Models.ApiKeySurrogate", b =>
                 {
